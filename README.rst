086bf7d038dd3a84d4cf343c4ddff07b42e0e7e2
Vinod Kurup <vinod@kurup.com>
2014-07-07 11:33:23 -0400

    Initial setup

---
+++ README.rst
@@ -0,0 +1,16 @@
+multitenancy
+========================
+
+Welcome to the documentation for django-multitenancy!
+
+
+Running the Tests
+------------------------------------
+
+You can run the tests with via::
+
+    python setup.py test
+
+or::
+
+    python runtests.py
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='django-multitenancy',
+    version=__import__('multitenancy').__version__,
+    author='<Include Your Name Here>',
+    author_email='<Include Your Email Here>',
+    packages=find_packages(),
+    include_package_data=True,
+    url='<Include Link to Project>',
+    license='<Include License Name>',
+    description=' '.join(__import__('multitenancy').__doc__.splitlines()).strip(),
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',      
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.rst'),
+    test_suite="runtests.runtests",
+    zip_safe=False,
+)
